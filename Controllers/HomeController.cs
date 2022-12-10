15fabd81037b08d934fe7c0cca3b32cc46ec5f2a
orbit <orbit@DESKTOP-J46P6Q8>
2022-12-10 14:33:54 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wasif_web_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
